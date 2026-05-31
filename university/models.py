4db454cea52b944d6a9a5e44fc0ef63bfa76c315
kkupschi <kkupschi@gmail.com>
2026-05-31 13:18:25 +0200

    feat: add Semester and Professor models with string representation

--- university/models.py
+++ university/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Semester(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Professor(models.Model):
+    name = models.CharField(max_length=100)
+    fachgebiet = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
